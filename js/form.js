6f5f7258d528f5f76f725b06a5678617a42089da
Pankajashree <pankajashree@gmail.com>
2017-05-31 21:29:38 +0530

    validate form

--- js/form.js
+++ js/form.js
@@ -1,15 +1,9 @@
 $(function() {
-    $('.error').hide();
     
     $("#submit").click(function() {
         // validate form 
-        $('.error').hide();
             var email = $("input#email").val();
-            if (email == "") {
-            $("label#email_error").show();
-            $("input#email").focus();
-            return false;
-        }
+    
             var message = $("#message").val();
             if (message == "") {
             $("label#message_error").show();
